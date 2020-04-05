667ce6ce76227edf327da339fed7b74bea400085
Andrey Borisovich <borisovich.andrey@gmail.com>
2020-04-05 23:25:44 +0200

    Added project files. Added example externals to copy. Added CopyLinuxExternals.targets script.

---
+++ Proj1/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "hello from Proj1!" << std::endl;
+    std::cout << "2";
+    return 0;
+}
---
+++ Proj2/main.cpp
@@ -0,0 +1,7 @@
+#include <cstdio>
+
+int main()
+{
+    printf("hello from Proj2!\n");
+    return 0;
+}
